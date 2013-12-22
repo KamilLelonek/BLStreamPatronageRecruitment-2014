60f6e5ab2d90e917ee2ab36f9c0cb42fae0b4b31
squixy <squixy.sln@gmail.com>
2013-12-22 10:09:43 +0100

    Second taks skeleton created.

---
+++ SecondTask/SecondTask/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SecondTask
+//
+//  Created by squixy on 22.12.2013.
+//  Copyright (c) 2013 squixy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
